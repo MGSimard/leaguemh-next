ca33b7c2a251be37ed9be50e188bde86c21d8784
MGSimard <g.marcgs@gmail.com>
2024-11-01 06:53:04 -0400

    Adjust config for testing

--- next.config.js
+++ next.config.js
@@ -13,6 +13,13 @@ const config = {
   devIndicators: {
     appIsrStatus: false,
   },
+  // TODO: REMOVE WHEN DONE
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default config;
